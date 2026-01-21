e6ab68bb191f8819bfc45a18d628314161942ce9
namratab-boop <namratab@testmu.ai>
2026-01-21 15:44:26 +0530

    new Update test_ios.py

--- tests/test_ios.py
+++ tests/test_ios.py
@@ -9,5 +9,5 @@ class TestLink:
         el1.click()
         
     def test_2(self):
-        el2 = self.driver.find_element(By.XPATH, "//*[@resource-id="com.lambdatest.proverbial:id/color"]")
+        el2 = self.driver.find_element(By.XPATH, '//*[@resource-id="com.lambdatest.proverbial:id/color"]')
         el2.click()
