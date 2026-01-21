2ff4fc9638e856cdedc516ed1c5430e4ec891c7c
namratab-boop <namratab@testmu.ai>
2026-01-21 14:47:13 +0530

    new Update test_android.py

--- tests/test_android.py
+++ tests/test_android.py
@@ -9,7 +9,6 @@ class TestLink:
         el1.click()
         
     def test_2(self):
-        def test_2(self):
         el2 = self.driver.find_element(By.XPATH, '//*[@resource-id="com.lambdatest.proverbial:id/color"]')
         el2.click()
     
